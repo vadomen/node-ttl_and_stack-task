00df9efc63f774e40cb6805612ae2e57899fd2e9
Daniel <danyjunor96@gmail.com>
2019-01-27 13:36:37 +0200

    Add changes

---
+++ tests/stack.js
@@ -0,0 +1,101 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const should = chai.should();
+
+const server = require('../testsServer.js');
+
+chai.use(chaiHttp);
+
+describe('Stack tests', () => {
+  describe('Add to stack', () => {
+    it('Add "Hello"', (done) => {
+      chai.request(server)
+        .post('/stack/add')
+        .send({
+          value: "Hello"
+        })
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          res.text.should.be.eql('Successfully added');
+          done();
+        });
+    });
+    it('Add "World"', (done) => {
+      chai.request(server)
+        .post('/stack/add')
+        .send({
+          value: "World"
+        })
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          res.text.should.be.eql('Successfully added');
+          done();
+        });
+    });
+    it('Add "Again"', (done) => {
+      chai.request(server)
+        .post('/stack/add')
+        .send({
+          value: "Again"
+        })
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          res.text.should.be.eql('Successfully added');
+          done();
+        });
+    });
+    it('No Value in request', function(done) {
+      chai.request(server)
+        .post('/stack/add')
+        .send({})
+        .end(function(err, res) {
+          expect(res).to.have.status(502);
+          res.text.should.be.eql('Please add value');
+          done();
+        });
+    });
+  });
+  describe('Get from stack', () => {
+    it('Get "Again"', (done) => {
+      chai.request(server)
+        .get('/stack/get')
+        .send()
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          res.text.should.be.eql('Again')
+          done();
+        });
+    })
+    it('Get "World"', (done) => {
+      chai.request(server)
+        .get('/stack/get')
+        .send()
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          res.text.should.be.eql('World')
+          done();
+        });
+    })
+    it('Get "Hello"', (done) => {
+      chai.request(server)
+        .get('/stack/get')
+        .send()
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          res.text.should.be.eql('Hello')
+          done();
+        });
+    })
+    it('Stack is empty', (done) => {
+      chai.request(server)
+        .get('/stack/get')
+        .send()
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          res.text.should.be.eql('')
+          done();
+        });
+    })
+  })
+})
---
+++ tests/store.js
@@ -0,0 +1,103 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const should = chai.should();
+
+const server = require('../testsServer.js');
+
+chai.use(chaiHttp);
+
+describe('Store tests', () => {
+  it('Set name John', (done) => {
+    chai.request(server)
+      .post('/store/add')
+      .send({
+        key: 'name',
+        value: "John"
+      })
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        res.text.should.be.eql('Successfully added');
+        done();
+      });
+  });
+  it('Get name', (done) => {
+    chai.request(server)
+      .get('/store/get?key=name')
+      .send()
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        res.text.should.be.eql('John');
+        done();
+      });
+  });
+  it('Get age', (done) => {
+    chai.request(server)
+      .get('/store/get?key=age')
+      .send()
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        res.text.should.be.eql('');
+        done();
+      });
+  });
+  it('Set name Larry with 5 sec ttl', (done) => {
+  chai.request(server)
+    .post('/store/add')
+    .send({
+      key: 'name',
+      value: "Larry",
+      ttl: 5000
+    })
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+      res.text.should.be.eql('Successfully added');
+      done();
+    });
+  });
+  it('Get name within 5 secconds', (done) => {
+    chai.request(server)
+      .get('/store/get?key=name')
+      .send()
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        res.text.should.be.eql('Larry');
+        done();
+      });
+  });
+
+  it('Delete age key from store', (done) => {
+    chai.request(server)
+      .delete('/store/delete?key=age')
+      .send()
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        res.text.should.be.eql('Successfully deleted');
+        done();
+      });
+  });
+
+  it('Get deleted key', (done) => {
+    chai.request(server)
+      .get('/store/get?key=age')
+      .send()
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        res.text.should.be.eql('');
+        done();
+      });
+  });
+
+  it('Get name after 6 secconds', (done) => {
+    setTimeout(()=>{
+    chai.request(server)
+      .get('/store/get?key=name')
+      .send()
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        res.text.should.be.eql('Larry');
+        done();
+      });
+    }, 6000);
+  });
+});
